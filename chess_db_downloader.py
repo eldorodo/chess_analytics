207172083b04875b7e8f777c2a6fb0c819a019a0
Sairam Subramanian Gopal <sasubra@microsoft.com>
2021-05-30 11:31:00 -0700

    script to download pgn convert to pgn, save it locally

--- chess_db_downloader.py
+++ chess_db_downloader.py
@@ -1,15 +1,22 @@
 import requests
 from multiprocessing import Pool
 from zipfile import ZipFile
+from converter.pgn_data import PGNData
 import os
-
-os.chdir("D:/Others/Personal projects/Sports analysis - git/chess_analytics")
 
 def download_url(url, save_path, chunk_size=128):
     r = requests.get(url, stream=True)
     with open(save_path, 'wb') as fd:
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
+
+
+def pgn_to_csv(pgn_file_name):
+
+    pgn_data = PGNData(pgn_file_name)
+    result = pgn_data.export()
+    result.print_summary()
+
 
 # Thread function
 def process_url(line_url):
@@ -22,19 +29,23 @@ def process_url(line_url):
         #continue
 
     print(f"Downloading {file_name}")
-    download_url(line_url, "Data/" + file_name)
-    
-
+    download_url(line_url, file_name)
 
     if (file_name.endswith("zip")):
 
-        with ZipFile("Data/"+ file_name, 'r') as zipObj:
+        with ZipFile(file_name, 'r') as zipObj:
             print(f"Extracting pgn file from zip file {file_name}")
-            zipObj.extractall("Data")
+            zipObj.extractall()
+            print (f"deleting zip file {file_name}")
+            os.remove(file_name)
 
-        print(f"Deleting {file_name}")
-        os.remove("Data/" + file_name)
-        
+    pgn_file_name = f"{file_name[:-4]}.pgn"
+    print(f"pgn file name: {pgn_file_name}")
+
+    pgn_to_csv(pgn_file_name)
+
+    print(f"Deleting {pgn_file_name}")
+    os.remove(pgn_file_name)
 
 # main starts here
 
@@ -43,7 +54,11 @@ f = open("master_download_links.txt", "r")
 all_lines = list(f.readlines())
 
 if __name__ == '__main__':
-    with Pool(15) as p:
+
+    if (os.path.exists('data') == False):
+        os.mkdir('data')
+
+    with Pool(20) as p:
         p.map(process_url, all_lines)
 
 f.close()
