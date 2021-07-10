11b4a00c8ac0263d44991fc23551508aff1640f0
Hariram Gopal <hariram.g@nagarro.com>
2021-07-11 02:15:31 +0530

    Trying to create table in DB

--- upload_to_AZ.py
+++ upload_to_AZ.py
@@ -1,12 +1,11 @@
-#https://docs.microsoft.com/en-us/azure/mysql/connect-python
-
+#https://docs.microsoft.com/en-us/azure/mysql/connect-python  => This way was not used in the code
 
 import pyodbc
 import pandas as pd
 server='chess-wiz.database.windows.net'
 database='pgnDB'
 username='adminchesswizz'
-password='Admin@12345'   
+password='Admin@12345'
 driver='{SQL Server}'
 
 #Remember to allow ur local IP in DB server firewall settings
@@ -18,3 +17,18 @@ try:
 except Exception as e:
   print("Error in connection")
   print(e)
+
+#https://www.dataquest.io/blog/sql-insert-tutorial/
+
+cursor = conn.cursor()
+
+sql_show = "SELECT * FROM contacts"
+
+sql_create = "CREATE TABLE contacts (" + \
+ 	"contact_id INT PRIMARY KEY);"
+
+#print(sql_create)
+
+cursor.execute(sql_create)
+
+
